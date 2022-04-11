26f343900ed4608c57f9c5a9e4e7583829cf83a1
pinaraltinisik <pinar.altinisik@student.griffith.ie>
2022-04-11 20:17:26 +0100

    login added

---
+++ static/app-setup.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdJDsHvoQLlQqWviEn6HCYMSyaFO6Ok68",
+  authDomain: "assignment2-346918.firebaseapp.com",
+  projectId: "assignment2-346918",
+  storageBucket: "assignment2-346918.appspot.com",
+  messagingSenderId: "691019755904",
+  appId: "1:691019755904:web:e9e4a048e6fbf257949165"
+};
+
+firebase.initializeApp(firebaseConfig);
